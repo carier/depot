a11c42d7c06e4eb3c81b4602cfcec21d9b4ff894
carier <carierboy@gmail.com>
2010-11-01 20:02:57 +0200

    =Tast T - Unit tests

--- test/functional/admin_controller_test.rb
+++ test/functional/admin_controller_test.rb
@@ -1,8 +1,36 @@
 require 'test_helper'
 
 class AdminControllerTest < ActionController::TestCase
+  fixtures :users
+
   # Replace this with your real tests.
   test "the truth" do
     assert true
   end
+  
+  test "index without user" do
+    get :index
+    assert_redirected_to :action => "login"
+    assert_equal "Please log in" , flash[:notice]
+  end
+
+  test "index with user" do
+    get :index, {}, { :user_id => users(:dave).id }
+    assert_response :success
+    assert_template "index"
+  end
+
+  test "login" do
+    dave = users(:dave)
+    post :login, :name => dave.name, :password => 'secret'
+    assert_redirected_to :action => "index"
+    assert_equal dave.id, session[:user_id]
+  end
+
+  test "bad password" do
+    dave = users(:dave)
+    post :login, :name => dave.name, :password => 'wrong'
+    assert_template "login"
+  end
+
 end
